6f865b2568880cc4636bb8d36df21a721697909e
k2moons <moonsocean@gmail.com>
2021-06-27 15:58:51 +0900

    update pacakge

--- Package.swift
+++ Package.swift
@@ -17,13 +17,14 @@ let package = Package(
     dependencies: [
         //.package(url: "https://github.com/BlueEventHorizon/BwLogger.git", from: "4.0.12"),
         .package(url: "https://github.com/BlueEventHorizon/BwLogger.git", .branch("main")),
+        .package(url: "https://github.com/BlueEventHorizon/InfoPlistKeys.git", .branch("main")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "BwNearPeer",
-            dependencies: ["BwLogger"]),
+            dependencies: ["BwLogger", "InfoPlistKeys"]),
         .testTarget(
             name: "BwNearPeerTests",
             dependencies: ["BwNearPeer"]),
